ed9bec8176f7580a1e3a1df35bf528d8e91da3c8
Karl Lapitan <karllapitan5@gmail.com>
2025-04-29 20:30:07 +0800

    edit some styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import ContactForm from "./ContactForm";
 
 const App = () => {
 return (
-  <div className="min-w-auto border h-screen bg-green-100 flex items-center justify-center">
+  <div className="max-w-screen border min-h-screen bg-green-100 flex items-center justify-center">
     <ContactForm /> 
+    
   </div>
 
 )
--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -9,12 +9,13 @@ const [dataForm , setDataForm] = useState({
     lastName:'',
     email:'',
     message:'',
+    queryType:'',
     checked:false,
     })
 
 
-// we track the radio input with state
-const [query, setQuery]=useState('');
+// // we track the radio input with state
+// const [query, setQuery]=useState('');
 
 const [error, setError] = useState(false);
 const [success, setSuccess] = useState(false);  
@@ -34,17 +35,17 @@ setDataForm({
 }
 
 const handleQueryChange = (e) => {
-setQuery(e.target.value)
+setDataForm({...dataForm , queryType:e.target.value})
 }
 
 const handleSubmit = (e) => {
 e.preventDefault();
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 if(!dataForm.firstName.trim() || 
-!dataForm.lastName || 
+!dataForm.lastName.trim() || 
 !emailRegex.test(dataForm.email) || 
-!dataForm.message.trim() ||
-!dataForm.checked
+!dataForm.message.trim() || 
+!dataForm.queryType || !dataForm.checked
   ) 
   {
   setError(true);
@@ -53,6 +54,8 @@ if(!dataForm.firstName.trim() ||
 }else {
 setError(false);
 setSuccess(true);
+console.log('Form submitted:', dataForm);
+setDataForm('')
 }
 
 
@@ -64,21 +67,22 @@ setSuccess(true);
 
 
 return (
-<>
+<div className="flex flex-col-reverse items-center justify-center w-min">
   <form 
-  className="border rounded-lg min-w-auto min-h-96  align-middle bg-FormBG p-8 "
+  className="border rounded-lg min-w-auto min-h-min  bg-FormBG p-4"
   onSubmit={handleSubmit}
   >
     <h2 className="font-Karla mb-2 font-semibold">Contact Us</h2>
 
-    <div className="flex flex-row gap-3" >
-    <label className="flex flex-col  font-Karla text-GrayDark w-48"> First Name  *
+    <div className="flex flex-col gap-0.5 w-min sm:flex-row mb-1" >
+    <label className="flex flex-col  font-Karla text-GrayDark w-48 text-sm "> First Name  *
+
 
     <input
     name='firstName'
     value={dataForm.firstName}
     onChange={handleInputChange}
-    className ={`border  ${error ? 'border-red-400' : 'border-GrayLight'}  hover:border-PrimaryGreenDark cursor-pointer focus:border-PrimaryGreenDark focus:outline-none p-1 rounded mt-2`}
+    className ={`border  ${error ? 'border-red-400' : 'border-GrayLight'}  hover:border-PrimaryGreenDark cursor-pointer focus:border-PrimaryGreenDark focus:outline-none p-1 rounded mt-1 w-min sm:w-46`}
 
     />
 
@@ -86,12 +90,12 @@ return (
     </label>
    
 
-    <label className="flex flex-col font-Karla text-GrayDark mb-2 w-48"> Last Name *
+    <label className="flex flex-col font-Karla text-GrayDark w-48 text-sm "> Last Name *
       <input 
       name='lastName'
       value={dataForm.lastName}
       onChange={handleInputChange}
-     className={`border  ${error ? 'border-red-400' : 'border-GrayLight'}  hover:border-PrimaryGreenDark cursor-pointer focus:border-PrimaryGreenDark focus:outline-none p-1 rounded mt-2`}
+     className={`border  ${error ? 'border-red-400' : 'border-GrayLight'}  hover:border-PrimaryGreenDark cursor-pointer focus:border-PrimaryGreenDark focus:outline-none p-1 rounded mt-1 w-min sm:w-47 `}
      />
       {error && <span className="text-red-500 text-sm mb-0 font-Karla">This field is required</span>}
       
@@ -99,56 +103,55 @@ return (
 
     </div>
 
-    <label className="flex flex-col text-GrayDark font-Karla mb-3 w-full " > Email Address *
+    <label className="flex flex-col text-GrayDark font-Karla mb-1  w-full " > Email Address *
       <input 
       name='email'
       value={dataForm.email}
       onChange={handleInputChange}
-      className={`border max-w-xl ${error ? 'border-red-400' :  'border-GrayLight'}  hover:border-PrimaryGreenDark cursor-pointer focus:border-PrimaryGreenDark focus:outline-none p-1 rounded mt-2 "
-      type="text" `} 
+      className={`border sm:w-sm ${error ? 'border-red-400' :  'border-GrayLight'}  hover:border-PrimaryGreenDark cursor-pointer focus:border-PrimaryGreenDark focus:outline-none p-1 rounded mt-1 w-min sm:w-lg`} 
       />
-      {error && <span className="text-red-500 text-sm mt-2 font-Karla">Please enter a valid Email address </span>}
+      {error && <span className="text-red-500 text-sm mt-1 font-Karla">Please enter a valid Email address </span>}
     </label>
     
     
     <fieldset className="flex flex-col "> 
-      <legend className="mb-2 font-Karla ">Query Type *</legend>
+      <legend className="mb-1 font-Karla ">Query Type *</legend>
 
-      <div className="flex flex-row gap-3 ">
+      <div className="flex flex-col gap-2 w-min  sm:flex-row sm:w-min">
       {/* for General Enquiry */}
     <label 
-  className={`flex items-center border border-GrayLight p-2 rounded text-sm w-48   hover:bg-PrimaryGreenLight ${query === 'generalEnquiry' ? 'bg-PrimaryGreenLight' : ''} font-Karla`}
+  className={`flex items-center border border-GrayLight p-2 rounded text-sm w-45 sm:w-46 hover:bg-PrimaryGreenLight ${dataForm.queryType === 'generalEnquiry' ? 'bg-PrimaryGreenLight' : ''} font-Karla`}
 >
         <input 
         type="radio" 
-        className="mr-2 mt-1 cursor-pointer accent-PrimaryGreenDark"
+        className="mr-2 mt-1 cursor-pointer accent-PrimaryGreenDark "
         name='queryType'
         value='generalEnquiry'
         onChange={handleQueryChange}
-        checked={query === 'generalEnquiry'}
+        checked={dataForm.queryType === 'generalEnquiry'}
         />
         General Enquiry
       </label>
 
       {/* for Support Request */}
         <label 
-       className={`flex items-center border border-GrayLight p-2 rounded text-sm w-48   hover:bg-PrimaryGreenLight ${query === 'supportRequest' ? 'bg-PrimaryGreenLight' : ''} font-Karla`}>
+       className={`flex items-center border border-GrayLight p-2 rounded text-sm w-45 sm:w-47 hover:bg-PrimaryGreenLight ${dataForm.queryType === 'supportRequest' ? 'bg-PrimaryGreenLight' : ''} font-Karla`}>
         <input 
-        className="mr-2 mt-1 cursor-pointer accent-PrimaryGreenDark font-Karla"
+        className="mr-2 mt-1 cursor-pointer accent-PrimaryGreenDark font-Karla "
         type="radio" 
         name='queryType'
         value='supportRequest'
          onChange={handleQueryChange}
-           checked={query === 'supportRequest'}
+           checked={dataForm.queryType === 'supportRequest'}
         />
         Support Request
       </label>
       </div>
-      {error && <span className="text-red-500 text-sm mb-2 mt-2 font-Karla">Please Select a query</span>}
+      {error && <span className="text-red-500 text-sm  mt-2 font-Karla">Please Select a query</span>}
 
 
-
-     <label className="font-Karla mt-3"> 
+{/* message */}
+     <label className="font-Karla mt-1"> 
     Message *
     <textarea
       name='message'
@@ -160,7 +163,7 @@ return (
     {error && <span className="text-red-500 text-sm mb-0 font-Karla ">This field is required</span>}
   </label>
 
-    <label className="flex items-center text-xs font-Karla mt-3   ">
+    <label className="flex items-center text-xs font-Karla mt-2   ">
       <input 
       className="mr-2 cursor-pointer accent-PrimaryGreenDark"
       type="checkbox" 
@@ -172,13 +175,13 @@ return (
 
 
     <button
-    className="border w-full p-3 rounded-md text-FormBG bg-PrimaryGreenDark font-Karla text-xs mt-3"
+    className="border w-full p-2 rounded-md text-FormBG bg-PrimaryGreenDark font-Karla text-xs mt-3"
     >Submit</button>
 
   </form>
 
 
-  {success && <div className="border rounded-lg  max-w-4xs py-4 px-4 absolute top-22 bg-GrayDark  text-FormBG  ">
+  {success && <div className="border rounded-lg  max-w-4xs py-4 px-4  bg-GrayDark  text-FormBG  ">
     
    <h2 className="font-bold font-Karla">Message Sent!</h2> 
    <p className="text-xs font-Karla">Thanks for completing the form. We'll be in touch soon!</p> 
@@ -188,7 +191,7 @@ return (
 
 
 
-</>
+</div>
 
 
 )
